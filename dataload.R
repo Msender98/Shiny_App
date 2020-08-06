4c71b355aef9d45ebe2346de60c52a99c62e39b6
Mike Sender <msender98@gmail.com>
2020-08-05 20:05:31 -0400

    Initialized Shiny App

--- dataload.R
+++ dataload.R
@@ -1,10 +1,10 @@
-library(jsonlite)
+library(jsonlite) 
 
 jsonfile = 'yelp_academic_dataset_business.json'
 
 yelp_business = stream_in(file(jsonfile))
-yelp_reviews = stream_in(file('yelp_academic_dataset_checkin.json'))
+yelp_checkins = stream_in(file('yelp_academic_dataset_checkin.json'))
 
 str(yelp_business)
 head(yelp_business)
-
+str(yelp_reviews)
